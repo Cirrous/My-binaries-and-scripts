f9c9ec704259181b5024ab49150bf0358fda3ede
Cirrous <christianraisch@gmail.com>
2026-09-08 19:11:06 +0200

    add overwrite menu to install-dotfiles script add color to interactive scripts

---
+++ bin/colors.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Shared color codes for interactive scripts
+
+BOLD='\033[1m'
+DIM='\033[2m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+CYAN='\033[0;36m'
+RESET='\033[0m'
